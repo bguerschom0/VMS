3dbc360615ae3c0b66fb2f5d9a81004ca14717be
bguerschom0 <bguerschom0@gmail.com>
2025-01-31 16:50:37 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,16 +16,26 @@ export default {
       },
       animation: {
         'float': 'float 4s ease-in-out infinite',
+        'float-reverse': 'float-reverse 4s ease-in-out infinite',
         'pulse': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'pulse-reverse': 'pulse-reverse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
       keyframes: {
         'float': {
           '0%, 100%': { transform: 'translateY(0)' },
           '50%': { transform: 'translateY(20px)' },
         },
+        'float-reverse': {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        },
         'pulse': {
           '0%, 100%': { transform: 'scale(1)' },
           '50%': { transform: 'scale(1.1)' },
+        },
+        'pulse-reverse': {
+          '0%, 100%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(0.9)' },
         }
       }
     },
