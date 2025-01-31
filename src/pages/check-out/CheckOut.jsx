4fc8b1fff7474abd44dfc4dc350b6f2071e21358
bguerschom0 <bguerschom0@gmail.com>
2025-02-01 00:49:24 +0200

    Update CheckOut.jsx

--- src/pages/check-out/CheckOut.jsx
+++ src/pages/check-out/CheckOut.jsx
@@ -91,7 +91,6 @@ const CheckOut = () => {
 
 return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
-      <Sidebar />
       
       <main className="pl-64">
         <div className="p-8">
