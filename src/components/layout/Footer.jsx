b84010cbf67b3b29d7babcb0e472ea84d226d653
bguerschom0 <bguerschom0@gmail.com>
2025-02-01 15:29:11 +0200

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,15 +1,15 @@
+import React from 'react'
+
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
-
   return (
-      <footer className="bg-gray-50 dark:bg-gray-900 py-2">
-        <div className="max-w-full px-4">
-          <p className="text-center text-sm text-gray-600 dark:text-gray-400">
-            © {new Date().getFullYear()} Bigirmana. All rights reserved.
-          </p>
-        </div>
-      </footer>
-  );
-};
+    <footer className="bg-gray-50 dark:bg-gray-900 py-2">
+      <div className="max-w-full px-4">
+        <p className="text-center text-sm text-gray-600 dark:text-gray-400">
+          © {new Date().getFullYear()} Bigirmana. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
 
-export default Footer;
+export default Footer
