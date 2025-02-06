0d7e48d0c15d105d389400b8a462bf2979d432ad
bguerschom0 <bguerschom0@gmail.com>
2025-02-06 10:01:10 +0200

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-gray-50 dark:bg-gray-900 py-2">
       <div className="max-w-full px-4">
         <p className="text-center text-sm text-gray-600 dark:text-gray-400">
-          © {new Date().getFullYear()} Bigirmana. All rights reserved.
+          © {new Date().getFullYear()} Bischom. All rights reserved.
         </p>
       </div>
     </footer>
