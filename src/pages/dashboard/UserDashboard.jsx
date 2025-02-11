c59f8fae5a62d1d1620251d8b4cc3e537c8402b9
bguerschom0 <bguerschom0@gmail.com>
2025-02-11 23:22:47 +0200

    Update UserDashboard.jsx

--- src/pages/dashboard/UserDashboard.jsx
+++ src/pages/dashboard/UserDashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../../hooks/useAuth';
 
 const UserDashboard = () => {
   const { user } = useAuth();
