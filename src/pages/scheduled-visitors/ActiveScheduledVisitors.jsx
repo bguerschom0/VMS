ecff9c03a4adb71baa16bb2e28df11ded732e5c2
bguerschom0 <bguerschom0@gmail.com>
2025-02-11 20:20:01 +0200

    Update ActiveScheduledVisitors.jsx

--- src/pages/scheduled-visitors/ActiveScheduledVisitors.jsx
+++ src/pages/scheduled-visitors/ActiveScheduledVisitors.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { supabase } from '../../config/supabase';
-import Sidebar from '../../components/layout/Sidebar';
 
 const ActiveScheduledVisitors = () => {
   const [visitors, setVisitors] = useState([]);
